10922240d75384b51ecff6fd25e05a5aa7afb16d
natan20200679 <natanaelmedrado@outlook.com>
2026-06-08 18:19:59 -0300

    Create methods for delete "clientes"

--- barbearia_pdv/pdv/urls.py
+++ barbearia_pdv/pdv/urls.py
@@ -7,7 +7,7 @@ from .views import (
     criar_cliente, criar_servico,
     register_view, logout_view,
     login_view, servicos_view,
-    dashboard, clientes_view, agendamentos_view, excluir_cliente,
+    dashboard, clientes_view, agendamentos_view, excluir_cliente, confirmar_excluir_cliente,
 )
 
 # Lista que armazena todas as rotas do app
@@ -28,6 +28,8 @@ urlpatterns = [
     path('clientes/criar/', criar_cliente, name='criar_cliente'),
     # Excluir cliente
     path('clientes/excluir/', excluir_cliente, name='excluir_cliente'),
+    path('clientes/excluir/<int:cliente_id>', excluir_cliente, name='excluir_cliente'),
+    path('clientes/confirmar/<int:cliente_id>', confirmar_excluir_cliente, name='confirmar_excluir_cliente'),
     # Lista de serviços
     path('servicos/', servicos_view, name='servicos'),
     # Criar serviço
